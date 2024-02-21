183df50b5c5f3e89c56e97065558097c1866ee63
Thomas Brennan <brennant1@ledyard.net>
2024-02-20 20:29:06 -0500

    First working code of drive train.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -8,7 +8,9 @@ import frc.robot.Constants.OperatorConstants;
 import frc.robot.commands.Autos;
 import frc.robot.commands.ExampleCommand;
 import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.subsystems.DriveTrain;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 
@@ -20,6 +22,7 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
  */
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
+  private final DriveTrain m_robotDrive = new DriveTrain();
   private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
@@ -30,6 +33,15 @@ public class RobotContainer {
   public RobotContainer() {
     // Configure the trigger bindings
     configureBindings();
+
+    // Configure default commands
+    // Set the default drive command to split-stick arcade drivem_robotDrive
+    m_robotDrive.setDefaultCommand(
+      // "Mario-Cart" drive: Triggers are gas and brake. Right stick turns left/right
+      // Triggers are Axis 2; RightStick X is axis 3
+      // Note the constants defined in the wpi XboxController class DO NOT MATCH the DS axes
+      new RunCommand(() ->
+        m_robotDrive.arcadeDrive(-m_driverController.getLeftY(), -m_driverController.getRightX(), true), m_robotDrive));
   }
 
   /**
